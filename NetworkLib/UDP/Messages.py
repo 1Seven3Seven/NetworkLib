0cb5810d0508d0a9dc6edb553c75a3dfbd96c27b
1Seven3Seven <pburgess2003@gmail.com>
2023-05-23 11:44:50 +0930

    DocStrings yay. I am a little lazy...

--- NetworkLib/UDP/Messages.py
+++ NetworkLib/UDP/Messages.py
@@ -9,36 +9,37 @@ from NetworkLib.Utils import get_local_ip
 class Messages:
     def __init__(self, port: int = 1024, ip: str = None):
         """
-        :param port: ...
-        :param ip: ...
+        :param port: The port number to bind the socket to.
+        :param ip: The IP address to bind the socket to. If not provided, the local IP address is used.
         """
 
         self.port: int = port
-        """..."""
+        """The port number to bind the socket to."""
 
         self.ip: str = get_local_ip() if ip is None else ip
-        """..."""
+        """The IP address to bind the socket to."""
 
         # Creating and binding our socket
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        """..."""
+        """The socket object for UDP communication."""
         self._socket.bind((self.ip, self.port))
 
         # Where we receive and store our messages
         self._receive_messages_thread: threading.Thread = None
-        """..."""
+        """The thread responsible for receiving and processing incoming messages."""
         self._received_messages: queue.Queue = queue.Queue()
-        """..."""
+        """A queue to store the received messages."""
 
         # If our thread should stop
         self._stop_event = threading.Event()
-        """..."""
+        """An event used to signal the thread to stop processing messages."""
 
     @property
     def thread_stopped(self) -> bool:
         """
-        ...
-        :return: ...
+        Returns a flag indicating if the _receive_messages_thread has stopped.
+
+        :return: True if the _receive_messages_thread has stopped, False otherwise.
         """
 
         return not self._receive_messages_thread.is_alive()
