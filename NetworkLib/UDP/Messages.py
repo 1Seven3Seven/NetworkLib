eba0e0df248700906bd4637a77e506df4c0a38cd
1Seven3Seven <pburgess2003@gmail.com>
2023-05-23 11:52:04 +0930

    Thread to listen for messages

--- NetworkLib/UDP/Messages.py
+++ NetworkLib/UDP/Messages.py
@@ -7,10 +7,11 @@ from NetworkLib.Utils import get_local_ip
 
 
 class Messages:
-    def __init__(self, port: int = 1024, ip: str = None):
+    def __init__(self, port: int = 1024, ip: str = None, receive_messages_timeout: float = 0.1):
         """
         :param port: The port number to bind the socket to.
         :param ip: The IP address to bind the socket to. If not provided, the local IP address is used.
+        :param receive_messages_timeout: The maximum time in seconds to wait for incoming messages.
         """
 
         self.port: int = port
@@ -34,6 +35,10 @@ class Messages:
         self._stop_event = threading.Event()
         """An event used to signal the thread to stop processing messages."""
 
+        # How long our thread should wait for messages
+        self._receive_messages_timeout = receive_messages_timeout
+        """The maximum time in seconds to wait for incoming messages."""
+
     @property
     def thread_stopped(self) -> bool:
         """
@@ -43,3 +48,22 @@ class Messages:
         """
 
         return not self._receive_messages_thread.is_alive()
+
+    def _receive_messages(self, stop_event: threading.Event) -> None:
+        """
+        Internal method to receive and process incoming messages.
+
+        This method is intended to be run in a separate thread.
+
+        :param stop_event: An event used to signal the thread to stop processing messages.
+        """
+
+        # While we are not to stop
+        while not stop_event.is_set():
+            # Check if there is a message to receive for the next x seconds
+            readable, _, _ = select.select([self._socket], [], [], self._receive_messages_timeout)
+            if len(readable) > 0:  # If there is a message
+                # Get the message, decode it, store it
+                message_bytes, address = self._socket.recvfrom(self.port)
+                message = message_bytes.decode("utf-8")
+                self._received_messages.put((message, address))
