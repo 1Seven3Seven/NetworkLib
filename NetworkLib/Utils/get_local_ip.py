227355133a016ecdfa6a034b453eef81207121e1
1Seven3Seven <pburgess2003@gmail.com>
2023-05-23 11:48:58 +0930

    Small DocString changes

--- NetworkLib/Utils/get_local_ip.py
+++ NetworkLib/Utils/get_local_ip.py
@@ -5,7 +5,9 @@ def get_local_ip() -> str:
     """
     This function attempts to obtain the local IP address of the machine by creating a temporary socket and connecting
     it to a known external IP address and port.
+
     If successful, it returns the local IP address associated with the socket.
+
     If the connection fails or encounters an error, it falls back to an alternative method by using
     `socket.gethostbyname(socket.gethostname())` to retrieve the local IP address.
 
