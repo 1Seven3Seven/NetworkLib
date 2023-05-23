6cb056197cd345051d76795a79665b35dfc68db8
1Seven3Seven <pburgess2003@gmail.com>
2023-05-23 11:33:50 +0930

    More work on UDP.Messages

--- UDP/Messages.py
+++ UDP/Messages.py
@@ -3,6 +3,8 @@ import select
 import socket
 import threading
 
+from Utils import get_local_ip
+
 
 class Messages:
     def __init__(self, port: int = 1024, ip: str = None):
@@ -14,6 +16,29 @@ class Messages:
         self.port: int = port
         """..."""
 
-        self.ip: str = socket.gethostbyname(socket.gethostname())
+        self.ip: str = get_local_ip() if ip is None else ip
         """..."""
 
+        # Creating and binding our socket
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        """..."""
+        self._socket.bind((self.ip, self.port))
+
+        # Where we receive and store our messages
+        self._receive_messages_thread: threading.Thread = None
+        """..."""
+        self._received_messages: queue.Queue = queue.Queue()
+        """..."""
+
+        # If our thread should stop
+        self._stop_event = threading.Event()
+        """..."""
+
+    @property
+    def thread_stopped(self) -> bool:
+        """
+        ...
+        :return: ...
+        """
+
+        return not self._receive_messages_thread.is_alive()
--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
-
+import UDP
+import Utils
