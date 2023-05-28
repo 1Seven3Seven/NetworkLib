2fd715e8caef3ee64b59daf52542c02e620bc962
1Seven3Seven <pburgess2003@gmail.com>
2023-05-28 23:48:28 +0930

    Preparing for receiving messages

--- NetworkLib/TCP/Server.py
+++ NetworkLib/TCP/Server.py
@@ -4,15 +4,22 @@ import threading
 from ipaddress import IPv4Address
 from typing import Union, Dict
 
+import select
+
 from NetworkLib.Utils import get_local_ip
 
 
 class Server:
-    def __init__(self, port: int = 1024, ip: str = None, receive_messages_timeout: float = 0.1):
+    def __init__(self, port: int = 1024, ip: str = None, receive_messages_timeout: float = 0.1,
+                 prepend_bytes_size: int = 4):
         """
         :param port: The port number to bind the socket to.
         :param ip: The IP address to bind the socket to. If not provided, the local IP address is used.
         :param receive_messages_timeout: The maximum time in seconds to wait for incoming messages.
+        :param prepend_bytes_size: The number of bytes used to prepend the message length when preparing the message.
+        This value determines the maximum message length that can be handled.
+        For example, if `prepend_bytes_size` is set to 4, the maximum message length is 2^32 - 1.
+        This is not enforced but will cause problems if not taken into consideration
         """
 
         self.port: int = port
@@ -49,6 +56,10 @@ class Server:
         self._receive_messages_timeout = receive_messages_timeout
         """The maximum time in seconds to wait for incoming messages."""
 
+        # The amount of bytes to use when preparing messages
+        self.prepend_bytes_size = prepend_bytes_size
+        """The number of bytes the message length is encoded to and prepended as"""
+
     @property
     def receiving_messages(self) -> bool:
         """
@@ -58,3 +69,24 @@ class Server:
         """
 
         return self._receive_clients_messages_threads is not None
+
+    def _receive_messages_from_client(self, stop_event: threading.Event, connection: socket.socket,
+                                      ip: IPv4Address) -> None:
+        """
+        Internal method to receive and process incoming messages from a client.
+
+        This method is intended to be run in a separate thread.
+
+        :param stop_event: An event used to signal the thread to stop processing messages.
+        """
+
+        while not stop_event.is_set():
+            # Check each client for messages and if any are found then store them
+            readable, _, _ = select.select([connection], [], [], self._receive_messages_timeout)
+            if len(readable) > 0:  # If there is a message
+                # Get the length
+                message_length = int.from_bytes(connection.recv(self.prepend_bytes_size), byteorder="big")
+                # Read the length
+                message_bytes = connection.recv(message_length)
+                # Store them
+                self._received_client_messages[ip].put(message_bytes.decode("UTF-8"))
